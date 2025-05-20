255d31d9d20b0c9aef84914e08e4b1211bd583c9
bridger <bridger@bridgers-MacBook-Air.local>
2025-05-19 23:10:16 -0400

    pomo 1.0.4

--- Formula/pomo.rb
+++ Formula/pomo.rb
@@ -1,7 +1,7 @@
 class Pomo < Formula
   desc "Terminal pomodoro timer with color config persistence"
   homepage "https://github.com/BrigBryu/pomo"
-  url      "https://github.com/BrigBryu/pomo/archive/refs/tags/v1.0.3.tar.gz"
+  url      "https://github.com/BrigBryu/pomo/archive/refs/tags/v1.0.4.tar.gz"
   sha256   "7c55e9c6557ca8195ebc81ca4023b9d5ddef6fdb0f5095132ae1c58dd5cb7cfe"
   license  "MIT"
 
